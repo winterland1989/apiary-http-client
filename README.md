adb4ad48261ac36494a07ecc5be00f683571431b
winterland <winterland1989@gmail.com>
2016-03-16 10:14:42 +0800

    copy method as well

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 Apiary HTTP Client
 ==================
 
+[![Hackage](https://img.shields.io/hackage/v/lens.svg?style=flat-square)](http://hackage.haskell.org/package/apiary-http-client)
+
 A HTTP Client for [Apiary](http://hackage.haskell.org/package/apiary), using `Apiary`'s extension api, suitable for proxying HTTP request to backend API, with flexible APIs and streamming proxying abilities.
 
-This module also reexport `Network.HTTP.Client`. Example:
+This module also reexport `Network.HTTP.Client`. 
+
+Example
+-------
 
 ```haskell
+import Web.Apiary
+import Network.Wai.Handler.Warp
+import Web.Apiary.HTTP.Client as HTTP
+
 main :: IO ()
 main = runApiaryWith (run serverPort) (HTTP.initHTTPClient HTTP.defaultManagerSettings) def $ do
 
@@ -18,11 +27,8 @@ main = runApiaryWith (run serverPort) (HTTP.initHTTPClient HTTP.defaultManagerSe
 
         -- set proxying host and port
         -- use function from Network.HTTP.Client to modify more
-        let req' = HTTP.setHost influxDB_API_HOST . HTTP.setPort 80 $ req
+        let req' = HTTP.setHost "api.backend.com" . HTTP.setPort 80 $ req
 
         -- send request and proxy respond in streamming fashion.
         HTTP.proxyTo req'
 ```
-
-
-More document are W.I.P.
--- Web/Apiary/HTTP/Client.hs
+++ Web/Apiary/HTTP/Client.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeOperators #-}
@@ -12,7 +11,7 @@ module Web.Apiary.HTTP.Client
     , initHTTPClient
     , getManager
     , withHTTPClient
-    -- ** helpers to make new Request
+    -- ** Helpers to make new Request
     , fromWaiRequest
     , fromRequest
     , resetHeaders
@@ -20,12 +19,12 @@ module Web.Apiary.HTTP.Client
     , setHost
     , setHostName
     , setHostHeader
-    -- ** send request and get respond
+    -- ** Send request and get respond
     , sendRequset
     , openRequset
-    -- ** send request for side effect
+    -- ** Send request for side effect
     , sendRequsetNoBody
-    -- ** send request and proxy respond
+    -- ** Send request and proxy respond
     , proxyTo
     , proxyWith
     , module Network.HTTP.Client
@@ -50,12 +49,12 @@ newtype HTTPClient = HTTPClient Manager
 
 instance Extension HTTPClient
 
--- |Initialize a @MonadExts@ with @Network.HTTP.Client.ManagerSettings@.
+-- |Initialize a @MonadExts@ with @ManagerSettings@.
 initHTTPClient :: MonadIO m
                => ManagerSettings -> Initializer m exts (HTTPClient ': exts)
 initHTTPClient ms = initializer' . liftIO $ newManager ms >>= return . HTTPClient
 
--- |Get @Network.HTTP.Client.Manage@ from Apiary's @MonadExts@ context.
+-- |Get @Manager@ from Apiary's @MonadExts@ context.
 getManager :: (Has HTTPClient es, MonadExts es m, MonadIO m) => m Manager
 getManager = do
     (HTTPClient manager) <- getExt (P.Proxy :: P.Proxy HTTPClient)
@@ -68,7 +67,7 @@ withHTTPClient f r = do
     manager <- getManager
     liftIO $ f r manager
 
--- |Copy path, headers, body and queryString from @Network.Wai.Request@
+-- |Copy path, headers, method, body and queryString from @Network.Wai.Request@
 fromWaiRequest
     :: ([T.Text] -> [T.Text])   -- ^ Function to modify request path
     -> ([Header] -> [Header])   -- ^ Function to modify request headers
@@ -85,6 +84,7 @@ fromWaiRequest pm hm req =
         def {
             queryString = W.rawQueryString req
         ,   path = T.encodeUtf8 (T.intercalate "/" path)
+        ,   method = W.requestMethod req
         ,   requestHeaders = headers
         ,   requestBody = requestBody'
         }
@@ -95,7 +95,7 @@ fromWaiRequest pm hm req =
 resetHeaders :: [Header] -> [Header]
 resetHeaders = filter (\ (name, _) -> name `notElem` [hTransferEncoding, hContentLength, hContentEncoding, hAcceptEncoding])
 
--- |Copy path, headers, body and queryString from current @ActionT@'s context.
+-- |Copy path, headers, method, body and queryString from current @ActionT@'s context.
 fromRequest
     :: (Has HTTPClient exts, MonadIO m)
     => ([T.Text] -> [T.Text])   -- ^ Function to modify request path
@@ -103,38 +103,43 @@ fromRequest
     -> A.ActionT exts prms m Request
 fromRequest pm hm= A.getRequest >>= return . fromWaiRequest pm hm
 
+-- |SetPort port req ＝ req{port = port}
 setPort :: Int -> Request -> Request
 setPort port req = req{ port = port }
 
+-- |setHostName host req ＝ req{host = host}
 setHostName :: B.ByteString -> Request -> Request
 setHostName host req = req{ host = host }
 
+-- |Set HOST header.
 setHostHeader :: B.ByteString -> Request -> Request
 setHostHeader host req = req{ requestHeaders = headers }
   where
     oHeaders = requestHeaders req
     headers = (hHost, host) : filter (\ (name, _) -> name /= hHost ) oHeaders
 
+-- |Set both HOST header and request's host.
 setHost :: B.ByteString -> Request -> Request
 setHost host =  setHostHeader host . setHostName host
 
--- |send requset and get @Response@ @ByteString@
+-- |Send requset and get @Response@ @ByteString@
 -- For large response consider using @openRequset@ and @responseClose@ instead.
 sendRequset :: (Has HTTPClient exts, MonadIO m)
     => Request -> A.ActionT exts prms m (Response LB.ByteString)
 sendRequset req = withHTTPClient httpLbs req
 
--- |send request without receive any body.
+-- |Send request without receive any body.
 sendRequsetNoBody :: (Has HTTPClient exts, MonadIO m)
     => Request -> A.ActionT exts prms m (Response ())
 sendRequsetNoBody req = withHTTPClient httpNoBody req
 
--- |send request and get @Response@ @BodyReader@
+-- |Send request and get @Response@ @BodyReader@
 openRequset :: (Has HTTPClient exts, MonadIO m)
     => Request -> A.ActionT exts prms m (Response BodyReader)
 openRequset req = withHTTPClient responseOpen req
 
--- |streamming response directly from proxy target.
+-- |Streamming response directly from proxy target.
+-- Reset headers automatically using @resetHeaders@.
 proxyTo :: (Has HTTPClient exts, MonadIO m)
     => Request -> A.ActionT exts prms m ()
 proxyTo req = do
@@ -153,6 +158,7 @@ proxyTo req = do
                 in loop
 
 -- |Modify response from proxy target then send.
+-- This method caches response body in memory so be careful.
 -- You should consider remove following headers:
 -- Transfer-Encoding, Content-Length, Content-Encoding and Accept-Encoding.
 proxyWith
